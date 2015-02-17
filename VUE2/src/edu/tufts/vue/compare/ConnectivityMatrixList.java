f4df72fc3de5f70e72a9daa43a71c90e6832dff6
freelsen <freelsen@gmail.com>
2015-02-17 10:16:01 +0800

    new orphan branch

---
+++ VUE2/src/edu/tufts/vue/compare/ConnectivityMatrixList.java
@@ -0,0 +1,30 @@
+
+/*
+* Copyright 2003-2010 Tufts University  Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package edu.tufts.vue.compare;
+
+import tufts.vue.LWLink;
+import tufts.vue.LWComponent;
+
+/*
+ * ConnectivityMatrixList.java
+ *
+ * Created on May 27, 2008, 10:22 AM
+ *
+ * Historical -- this class now just serves as a typed ArrayList -- could be tossed.  SMF 2012
+ */
+public final class ConnectivityMatrixList extends java.util.ArrayList<ConnectivityMatrix> {}
+    
